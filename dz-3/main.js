7b8a59d86cf267407eac78eab1eaa15da2379ebe
Артур Валиев <lordkg07@gmail.com>
2024-12-16 17:49:37 +0600

    Add files via upload

---
+++ dz-3/main.js
@@ -0,0 +1,96 @@
+// let input = [1, 4, 3,]
+
+
+// function inputX2(input) {
+//     return input.map(x => x * 2)
+// }
+// console.log(inputX2(input));
+
+
+
+
+
+// let input = [1, -2, 6, -9, -4,]
+
+
+// function number(input) {
+//   return input.map(x => (x === 0 ? 0 : -x))
+// }
+// console.log(number(input));
+
+
+
+
+
+
+
+// let input = [4, 6, 'Ivan', 5, 'Denis ']
+
+
+// function stringNumber(input) {
+//     return input.map(x => (typeof x === 'string' ? null : x))
+// }
+// console.log(stringNumber(input));
+
+
+
+
+
+
+
+// let input = ['Евдоким','Ivan', 'Игнат', 'Denis']
+
+
+// function lenghtName(input) {
+//     return input.filter (name => name.length <= 5)
+// }
+// console.log(lenghtName(input));
+
+
+
+
+
+
+
+// let input = [3, 5, 1, 6,]
+
+
+// function numKub(input) {
+//     return input.map(x => x ** 3)
+// }
+// console.log(numKub(input));
+
+
+
+
+
+
+
+// let input = ['Термос', 'Ураган', 'Земля', 'Комбо', 'Корень']
+
+
+// function numberString(input) {
+//     return input.map (x => x.length)
+// }
+// console.log(numberString(input));
+
+
+
+
+
+// эту не смог (
+
+// let input = [1, 2, 3, 4, 5 ] 
+
+
+// function numberP(input) {
+//     return input.map (x => `Цифрф ${input}`)
+// }
+
+// numberP.forEach(el => {
+//     let p = document.createElement('p')
+//     p.textContent = text;
+//     document.body.appendChild(p)
+// });
+
+// numberP(input)
